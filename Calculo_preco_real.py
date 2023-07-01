2953cdf353b752cac1da9df77ce33f1555668a22
Sagi <santos.sagitc@gmail.com>
2023-07-01 01:19:49 -0300

    Deixando o código mais limpo, organizado

--- Calculo_preco_real.py
+++ Calculo_preco_real.py
@@ -1,64 +1,84 @@
 import os
 
-print('Bem-vindo ao programa que calcula o valor real da nota e dos produtos')
-print('Troque "," por "."')
-print('para sair do programa digite "sair".')
+def clearTerminal():
+    os.system('cls')
 
-diferenca_nota = 0
-user_answer = ""
+def apresentation():
+    clearTerminal()
+    print('Bem-vindo ao programa que calcula o valor real da nota e dos produtos')
+    print('Troque "," por "."')
+    print('para sair do programa digite "sair".')
+
+def exitProgram(answer):
+    if answer == "sair":
+        return quit()
+
+def resultCalculation(valor_produtos, valor_nota):
+    try:
+        if float(valor_nota) > float(valor_produtos):
+            result = ((float(valor_nota) * 100)/float(valor_produtos))-100
+            return result
+        elif float(valor_nota) < float(valor_produtos):
+            result = ((float(valor_nota) * 100)/float(valor_produtos))-100
+            return result
+        else:
+            return print('\nNão houve desconto ou acréscimo nessa nota.')
+    except:
+        return print('\nTroque "," por "."')
+
+def taxesProducts(preco_produto, quantidade_produto, diferenca_nota):
+    result = (preco_produto/quantidade_produto)
+    result = result + (result*(diferenca_nota/100))
+    return result
+
+apresentation()
 
 while True:
     
-    difer_total_prod = input('\ninsira o valor total dos produtos: ')
-    if difer_total_prod == "sair":
-        break
-    difer_total_nota = input('Insira o valor total da nota: ')
-    if difer_total_nota == "sair":
-        break
+    total_produtos = input('\ninsira o valor total dos produtos: ')
+    exitProgram(total_produtos)
+    total_nota = input('Insira o valor total da nota: ')
+    exitProgram(total_nota)
 
-    try:
-        valor_prod = float(difer_total_prod)
-        valor_nota = float(difer_total_nota)
-    except:
-        print("\nDigite apenas números, troque ',' por '.'")
+    result = resultCalculation(total_produtos, total_nota)
+
+    if result == None:
         continue
-
-    if valor_nota > valor_prod:
-        diferenca_nota = ((valor_nota * 100)/valor_prod)-100
-        print(f'\nHá um acréscimo de {diferenca_nota:,.0f}%')
-    elif valor_nota < valor_prod:
-        diferenca_nota = ((valor_nota * 100)/valor_prod)-100
-        print(f'\nHá um desconto de {diferenca_nota:,.0f}%')
     else:
-        print('\nNão houve desconto ou acréscimo nessa nota.')
-        continue
+        if result < 0:
+            print(f'\nHouve um desconto de {result:,.0f}%')
+        else:
+            print(f'\nHouve um acréscimo de {result:+,.0f}%')
     
     user_answer = input('\nDeseja calcular o preço real de cada produto? Sim ou Não.  ').lower()
 
-    if user_answer == "sair":
-        break
-    elif user_answer == "sim":
-        os.system('cls')
+    exitProgram(user_answer)
+
+    if user_answer == "sim" or user_answer == "yes" or user_answer == "y" or user_answer == "s":
+        clearTerminal()
+
         print('\nDigite "sair" a qualquer momento para parar de calcular o preço real dos produtos.')
 
         while user_answer != "sair":
 
             user_answer = input('\nInforme o preço do produto: ')
-            if user_answer == "sair":
-                continue
+            exitProgram(user_answer)
             preco_prod = float(user_answer)
+
             user_answer = input('informe a quantidade que veio o produto: ')
+            exitProgram(user_answer)
             quant_prod = int(user_answer)
 
-            preco_real_prod = (preco_prod/quant_prod)
-            preco_real_prod = preco_real_prod + (preco_real_prod*(diferenca_nota/100))
+            preco_real = taxesProducts(preco_prod, quant_prod, result)
 
-            print(f'O preço real desse produto é: {preco_real_prod:,.2f}')
+            print(f'O preço real desse produto é: {preco_real:,.2f}')
         else:
             break
 
-    elif user_answer == "não":
-        break
+    elif user_answer == "não" or user_answer == "nao" or user_answer == "no" or user_answer == "n":
+        apresentation()
+        continue
     else:
-        print('Resposta inválida.')
+        print('\nResposta inválida.')
+        break
 
